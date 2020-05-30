2689783097960d20458c2d0c335102b3cecfc19a
Duegan24 <62625959+Duegan24@users.noreply.github.com>
2020-05-29 21:41:10 -0500

    Updated based on feedback

--- challenge.py
+++ challenge.py
@@ -98,19 +98,19 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
 
 
     try:
-        # Load Wiki Data
+        # Load Wiki Data - Assumption is that the Wiki file is a JSON formated file
         with open(wiki_data_path, mode='r') as file:
             wiki_movies_raw = json.load(file)
-        # Load Kaggle Metadata
+        # Load Kaggle Metadata  - Assumption is that the Kaggle Metadata file is a csv file
         kaggle_metadata = pd.read_csv(kaggle_metadata_path, low_memory=False)
-        # Load Kaggle Rating Data
+        # Load Kaggle Rating Data  - Assumption is that the ratings data is a csv file
         ratings = pd.read_csv(kaggle_rating_data_path)
     except:
         e = sys.exc_info()[0]
         print( "Error while loading data: %s" % e )
 
     try:
-        # Clean Wiki Movies
+        # Clean Wiki Movies - Primary assumuptions here are that the column headings are identical - header cleaning assumes specific names
         ## Only include those records that have a director, imdb_link and does not include number of episodes
         wiki_movies = [movie for movie in wiki_movies_raw
                 if ('Director' in movie or 'Directed by' in movie)
@@ -123,14 +123,14 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         ## Remove Duplicates from Clean Movies Dataframe
         wiki_movies_df['imdb_id'] = wiki_movies_df['imdb_link'].str.extract(r'(tt\d{7})')
         wiki_movies_df.drop_duplicates(subset='imdb_id', inplace=True)
-        ## Remove columns that have values in less than 10% of the records
+        ## Remove columns that have values in less than 10% of the records - Assumption here is that the 10% that are removed are not useful
         clean_columns_to_keep = [column for column in wiki_movies_df.columns if wiki_movies_df[column].isnull().sum() < len(wiki_movies_df)*0.9]
         wiki_movies_df = wiki_movies_df[clean_columns_to_keep]
 
         # Box Office values clean up
         box_office = wiki_movies_df['Box office'].dropna()
         box_office = box_office.apply(lambda x: ' '.join(x) if type(x) == list else x)
-        ## Define the regular expressions that we will use to clean up the box office revenue
+        ## Define the regular expressions that we will use to clean up the box office revenue - Assumptions here is that a very small fraction have forms that are not covered here.
         form_one = r'\$\s*\d+\.?\d*\s*[mb]illi?on'
         form_two = r'\$\s*\d{1,3}(?:[,\.]\d{3})+(?!\s[mb]illion)'
         ## Replace ranges of values with one value
@@ -156,7 +156,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         # Clean Up Release Date
         release_date = wiki_movies_df['Release date'].dropna().apply(lambda x: ' '.join(x) if type(x) == list else x)
         ## Define the regular expressions that we will use to clean up the release dates
-        ### Month Name two digit or one digit day comma and four digit year
+        ### Month Name two digit or one digit day comma and four digit year - Assumptions here is that a very small fraction have forms that are not covered here.
         date_form_one = r'(\b(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\D?(\d{1,2}\D?)?\D?([12]\d{3}))'
         date_form_two = r'(([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])))'
         date_form_three = r'(\b(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?)\s*([12]\d{3}))'
@@ -168,7 +168,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
 
         # Clean Running Time
         running_time = wiki_movies_df['Running time'].dropna().apply(lambda x: ' '.join(x) if type(x) == list else x)
-        ## Created a datatable of the hour + minutes or just minutes. Each round bracket creates a new column in the datatable
+        ## Created a datatable of the hour + minutes or just minutes. Each round bracket creates a new column in the datatable - Assumptions here is that a very small fraction have forms that are not covered here.
         running_time_extract = running_time.str.extract(r'(\d+)\s*ho?u?r?s?\s*(\d*)|(\d+)\s*m')
         ## Fill all NaN values with zero...the to_numeric is converting the strings to numeric values if it is not then it will throw an error
         ## which is then replaced with a Na and the fillna is chained in to replace the na with a zero
@@ -182,7 +182,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         print( "Error Cleaning Wiki Data: %s" % e )
 
     try:
-        # Kaggle Data Cleanup
+        # Kaggle Data Cleanup - Assumption is that all the column names do not change
         kaggle_metadata = kaggle_metadata[kaggle_metadata['adult'] == 'False'].drop('adult',axis='columns')
         ## Column Cleaning to correct data types
         kaggle_metadata['video'] = kaggle_metadata['video'] == 'True'
@@ -213,7 +213,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         movies_df.drop('video',axis=1,inplace=True)
 
         # Final Restructuring/renaming of columns
-        ## Reorder the Columns to be more useful
+        ## Reorder the Columns to be more useful  - Assumption is that all the column names do not change
         movies_df = movies_df.loc[:, ['imdb_id','id','title_kaggle','original_title','tagline','belongs_to_collection','url','imdb_link',
                             'runtime','budget_kaggle','revenue','release_date_kaggle','popularity','vote_average','vote_count',
                             'genres','original_language','overview','spoken_languages','Country',
@@ -242,7 +242,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         print( "Error while merging into Movies Dataframe: %s" % e )
 
     try:
-        # Rating data cleanup
+        # Rating data cleanup - Assumption is that all the column names do not change
         ratings['timestamp'] = pd.to_datetime(ratings['timestamp'], unit='s')
         ## Group the data by movieID and rating, rename teh userID to count...since that is what it is now,
         ## and pivot so the index is now movie ID
@@ -259,7 +259,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         print( "Error while cleaning and merging Ratings data: %s" % e )
 
     try:
-        #  Connect to postgres server
+        #  Connect to postgres server  - Assumptin is that the database file is movie_data
         connection_string = f"postgres://postgres:{db_password}@localhost:5432/movie_data"
         engine = create_engine(connection_string)
 
@@ -267,7 +267,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         engine.delete("movies")
 
         # Push Movies DataFrame to postgres database
-        movies_df.to_sql(name='movies', con=engine)
+        movies_df.to_sql(name='movies', if_exists='append', con=engine)
     except:
         e = sys.exc_info()[0]
         print("Error while sending Movies DataFram to postgres: %s" % e )
@@ -278,7 +278,7 @@ def movie_data_cleaner_to_postgres(wiki_data_path, kaggle_metadata_path, kaggle_
         start_time = time.time()
         for data in pd.read_csv(kaggle_rating_data_path, chunksize=1000000):
             print(f'importing rows {rows_imported} to {rows_imported + len(data)}...', end='')
-            data.to_sql(name='ratings', con=engine, if_exists='append')
+            data.to_sql(name='ratings', con=engine, if_exists='replace')
             rows_imported += len(data)
             # add elapsed time to final print out
             print(f'Done. {time.time() - start_time} total seconds elapsed')
